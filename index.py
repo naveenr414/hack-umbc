0357c1caf4417ae0fdb6af1e1647c23216bedec2
naveenr414 <dsfan414@gmail.com>
2018-10-20 16:18:59 -0400

    Added in debugging to Flask

--- index.py
+++ index.py
@@ -1,9 +1,13 @@
 from flask import Flask
 app = Flask(__name__)
 
+DEBUG = True
+
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
 
 if __name__ == '__main__':
-   app.run()
+    app.debug = DEBUG
+    app.run()
+    app.run(debug = DEBUG)
