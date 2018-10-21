8421b74d7bd8f903673d415e426c5034678c969d
naveenr414 <dsfan414@gmail.com>
2018-10-20 20:29:09 -0400

    Added in candidate info to scraper

--- web-scraper/scraper.py
+++ web-scraper/scraper.py
@@ -1,14 +1,15 @@
-from mongoscraper import populate
 from bs4 import BeautifulSoup
 import urllib.request as ur
 import json
-import certifi
 
 partyOneLetter = {"D":"Democrat","R":"Republican","I":"Independent"}
+writeMongo = False
+if(writeMongo):
+    from mongoscraper import populate
 
 def getData():
     jsonList = []
-    certifi.where()
+    
     electionWebsite = "https://www.electoral-vote.com/evp2018/Senate/senate_races.html"
     electionSoup = BeautifulSoup(ur.urlopen(electionWebsite),"html.parser")
 
@@ -45,16 +46,46 @@ def getData():
                     party = partyOneLetter[t[1]]
                     candidateJson["party"] = party
 
-            candidateList.append(candidateJson)                    
+
+            infoLink = "http://www.ontheissues.org/Senate/"
+            infoLink+=name.replace(" ","_")+".htm"
+
+            print(infoLink)
+
+            works = True
+            
+            try:
+                ur.urlopen(infoLink)
+            except:
+                works = False
+
+            fields = range(1,21)
+            fields = list(map(lambda x: "position_"+str(x),fields))
+
+            for field in fields:
+                candidateJson[field] = "Unknown"
+
+            if(works):
+                candidateSoup = BeautifulSoup(ur.urlopen(infoLink),"html.parser")
+                tableList = candidateSoup.findAll("tr")
+
+                for row in tableList:
+                    if(row.find("a")!=None and row.find("a").has_attr("name") and row.find("a")["name"][0]=="q"):
+                        num = row.find("b").find("a")["name"][1:]      
+                        stance = row.find("b").text.strip()+" the idea that "+row.findAll("a")[1].text
+                        candidateJson["position_"+num] = stance
+        
+            candidateList.append(candidateJson)
 
         tempJson["candidates"] = candidateList
-        populate.write(tempJson)
+
+        if(writeMongo):
+            populate.write(tempJson)
         
         allJsons.append(tempJson)
             
         
     return allJsons
 
-populate.deletes()
+
 c = getData()
-populate.prints()
