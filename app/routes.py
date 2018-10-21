2cb536dd747a8de0884d55dadf053787f71fe7ed
naveenr414 <dsfan414@gmail.com>
2018-10-21 10:02:08 -0400

    Updated routes

--- app/routes.py
+++ app/routes.py
@@ -1,3 +1,20 @@
+Skip to content
+ 
+Search or jump to…
+
+Pull requests
+Issues
+Marketplace
+Explore
+ @naveenr414 Sign out
+3
+0 1 naveenr414/hack-umbc
+ Code  Issues 0  Pull requests 0  Projects 0  Wiki  Insights  Settings
+hack-umbc/app/routes.py
+c54db94  3 hours ago
+@naveenr414 naveenr414 Added house and governor to the output from address
+      
+47 lines (38 sloc)  1.34 KB
 from app import app
 from flask import render_template, redirect, url_for, request,session, request, jsonify
 from .forms import AddressForm
@@ -24,6 +41,23 @@ def address():
         address = args["address"]
     elif("address" in forms):
         address = forms["address"]
+    elif("lat" in forms and "lon" in forms):
+        address = apiscraper.reverseGeocode(forms["lat"],forms["lon"])
+    elif("lat" in args and "lon" in args):
+        address = apiscraper.reverseGeocode(forms["lat"],forms["lon"])
     state, district = apiscraper.findGeo(address)
+    races = {}
+    races["house"] = []
+    races["senate"] = []
+    races["governor"] = []
+
+    for x in find.query(state, district)[0]:
+        races["house"].append(dict(x))
+
     for x in find.query(state, district)[1]:
-        return dumps(dict(x))
+        races["senate"].append(dict(x))
+
+    for x in find.query(state, district)[2]:
+        races["governor"].append(dict(x))
+
+    return dumps(dict(races))
